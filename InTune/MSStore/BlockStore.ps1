fd7acb251665fc5cbd35bd31393cc40ae73c55c4
Philip Marsh <130765345+Marshyp@users.noreply.github.com>
2024-09-04 15:03:56 +0100

    Create BlockStore.ps1

---
+++ InTune/MSStore/BlockStore.ps1
@@ -0,0 +1,20 @@
+try {
+    # Define the registry path
+    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
+
+    # Define the name and value for the registry key
+    $registryName = "RequirePrivateStoreOnly"
+    $registryValue = 1
+
+    # Check if the registry path exists, create it if it doesn't
+    if (-not (Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force
+    }
+
+    # Set the registry key value
+    Set-ItemProperty -Path $registryPath -Name $registryName -Value $registryValue -Type DWord
+
+    Write-Host "Registry key set successfully."
+} catch {
+    Write-Host "An error occurred: $_"
+}
